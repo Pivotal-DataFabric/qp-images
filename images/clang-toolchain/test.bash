eb92f09ce4982bc2549e591403616ec24a286db2
Chris Hajas <chajas@vmware.com>
2021-05-03 14:33:26 -0400

    Add clang-tidy to clang-toolchain

--- images/clang-toolchain/test.bash
+++ images/clang-toolchain/test.bash
@@ -3,14 +3,18 @@
 set -e -u -o pipefail
 
 it_has_executables() {
+	type -p cmake
 	type -p clang-format-11
+	type -p clang-tidy-11
 	type -p xargs
 	type -p git
 	type -p parallel
+	type -p ninja
 }
 
 _main() {
 	it_has_executables
+	ldconfig -p | grep libxerces
 }
 
 _main "$@"
