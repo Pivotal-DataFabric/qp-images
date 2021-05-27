47452c606a6d5a56cc5282389de2a073b63600bd
Jesse Zhang <sbjesse@gmail.com>
2021-05-27 14:00:03 -0700

    Add flex and bison to (minimally) build Greenplum
    This makes the configure script happy when we disable a whole bunch of
    unrelated features and enables building `src/backend/gpopt`.

--- images/clang-toolchain/test.bash
+++ images/clang-toolchain/test.bash
@@ -3,9 +3,12 @@
 set -e -u -o pipefail
 
 it_has_executables() {
+	type -p bison flex
+	type -p ccache
 	type -p cmake
 	type -p clang-format-11
 	type -p clang-tidy-11
+	type -p clang++-11 clang-11
 	type -p xargs
 	type -p git
 	type -p parallel
